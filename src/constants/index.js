83e3d220d2f3d34409a76d241c5c224788e8b992
Bert Lievrouw <bert.lievrouw@gmail.com>
2020-03-28 11:40:07 +0100

    Use build folder

--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const INSTAGRAM_URL = 'https://www.instagram.com/kazverr/';
-export const FACEBOOK_URL = 'https://www.facebook.com/kaz.verstraete';
-export const STRAVA_URL = 'https://www.strava.com/athletes/8970964';
-
